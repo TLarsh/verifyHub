a64404299f73470557ac42279de22e3653089ae9
ADEYEMI AYOBAMI SAMUEL <107284584+TLarsh@users.noreply.github.com>
2025-01-21 06:58:44 +0100

    Add files via upload
    changed mongoose config

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,12 +1,34 @@
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
+require('dotenv').config(); // Only needed if you're testing locally with a .env file
+
+mongoose.set('strictQuery', false);
+
 
 const dbConnect = () => {
-    try{
-        const conn = mongoose.connect(process.env.MONGODB_URL);
-        console.log('Database conected Successfully.');
-    } catch (error) {
-        console.log('Server error')
+
+    const dbUri = process.env.MONGODB_URL;
+
+    if (!dbUri) {
+      console.error('MONGODB_URL is not defined. Please set it in your environment variables.');
+      process.exit(1); // Stop the app if no URI is provided
     }
+
+    mongoose.connect(dbUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+      .then(() => console.log('Connected to MongoDB'))
+      .catch((err) => console.error('Error connecting to MongoDB:', err));
+    // try{
+    //     const conn = mongoose.connect(process.env.MONGODB_URL);
+    //     console.log('Database conected Successfully.');
+    // } catch (error) {
+    //     console.log('Server error')
+    // }
 };
 
 module.exports = dbConnect
+
+
+
+
